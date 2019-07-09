3de16eecb2de0cb25139a77f8fd8db289a7e5071
jpdef <jacobdefilippis@gmail.com>
2019-07-08 18:46:51 -0700

    continuting to construct and then invert dispersion relation, in iwf. mode zero still flat, k range is tricky to guess

--- iw_field.py
+++ iw_field.py
@@ -22,8 +22,8 @@ class InternalWaveField:
         self.range = np.linspace(0,1,npoints[1])
         self.vmodes = iwvm.iw_vmodes(bfrq,self.depth)
         self.num_modes = num_modes
-        #Construct a set of dummy wave numbers from range
-        self.hwavenumbers = [20*np.pi/(wl+0.1) for wl in self.range] 
+        self.hwavenumbers = np.zeros([self.num_modes,len(freqs)])
+
 
 
     def construct_field(self):
@@ -35,7 +35,18 @@ class InternalWaveField:
         pass
 
 
-    def construct_dispersion_curves(self):
+    def construct_hwavenumbers(self,freqs):
+        #Construct a set of dummy wave numbers from range
+        hwavenumbers = [20*np.pi/(wl+0.1) for wl in self.range]
+        
+        #Make a interploations of dispersion for each mode
+        self.construct_dispersion_curves(hwavenumbers)
+
+        #Invert interpolation to compute wavenumber from input frequency
+        self.hwavenumbers = self.transform_frequencies_to_wavenumber(freqs) 
+
+
+    def construct_dispersion_curves(self,hwavenumbers):
         """
         Desc:
         Interpolates a dispersion curve from the eigenvalues of
@@ -43,14 +54,15 @@ class InternalWaveField:
         """
         self.dispcurves=[]
         for m in range(self.num_modes):
-            omegas = []
-            for wn in self.hwavenumbers:
+            freqs = []
+            for wn in hwavenumbers:
                 cp,vr = self.vmodes.gen_vmodes_evp(wn)
-                omegas.append( np.sqrt(cp[m].real)*wn )
-            f = interpolate.interp1d(self.hwavenumbers,omegas)
+                freqs.append( np.sqrt(cp[m].real)*wn )
+            f = interpolate.interp1d(hwavenumbers,freqs)
             self.dispcurves.append(f)
 
-    def transform_to_k(omega,m):
+
+    def transform_frequencies_to_wavenumber(self,freqs):
         """
         Desc:
         Transforms frequency to horizontal wave number through 
@@ -59,4 +71,28 @@ class InternalWaveField:
             omega : frequency of the wave
             m     : the vertical mode number
         """
-        pass 
+        hwavenumbers = np.zeros([self.num_modes,len(freqs)])
+        for m in range(self.num_modes):
+            for f,freq in enumerate(freqs):
+                hwavenumbers[m,f] = self.invert_for_frequency(freq,m)
+        return hwavenumbers
+
+
+    def invert_for_frequency(self,freq,m):
+        """
+        Desc:
+        Transforms an individual frequncy to a wavenumber via
+        dispersion curve that has been generated
+        """
+        krange = [20*np.pi/(wl+0.1) for wl in self.range]
+        if self.dispcurves is None:
+           self.construct_dispersion_curves()
+        f =  self.dispcurves[m]
+        fshift = lambda x : f(x) - freq
+        try:
+            sol = scipy.optimize.fsolve(fshift,krange[-1])
+            print(sol)
+            return sol
+        except ValueError:
+            return -1
+        

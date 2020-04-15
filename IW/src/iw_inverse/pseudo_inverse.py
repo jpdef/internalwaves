59a5a2ca54291a0e3d6b39d995777421a7fef8c4
jpdef <jacobdefilippis@gmail.com>
2020-04-15 15:18:27 -0700

    updated inversions to efficiently use diagonal matrices

--- IW/src/iw_inverse/pseudo_inverse.py
+++ IW/src/iw_inverse/pseudo_inverse.py
@@ -1,10 +1,17 @@
 import numpy as np 
 
-def tappered_least_square(H,RINV=None,QINV=None):    
-    RINV = RINV if RINV is not None else np.diag(np.ones(H.shape[0]))
+def tappered_least_square(H,RINV=None,QINV=None):
     QINV = QINV if QINV is not None else np.diag(np.ones(H.shape[1]))
     
-    HDAG  = H.T @ RINV @ H + QINV
-    HDAG  = np.linalg.inv(HDAG) @ H.T @ RINV
+    if RINV is not None:
+        M = np.sqrt(RINV) * H
+        HDAG = np.linalg.inv(M.T @ M + np.diag(QINV) ) @ M.T * np.sqrt(RINV)
+        
+    else:
+        HDAG = np.linalg.inv(H.T @ H + np.diag(QINV) ) @ H.T
+        
+        
+    RINV = RINV if RINV is not None else np.diag(np.ones(H.shape[0]))
     
     return HDAG
+
